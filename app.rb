eb39ce260f16c69200408f2c973cc06ec800d94e
Tania Barroso López <tbarrosolopez@gmail.com>
2016-12-21 15:20:37 -0500

    usando layout en la pagina inicio

--- app.rb
+++ app.rb
@@ -1,4 +1,12 @@
 require 'sinatra'
+
+
  get '/index' do
       'Hi!!, Mis primeros pasos en Ruby - Sinatra'
  end
+ get '/inicio' do
+	erb:'inicio', :layout =>("/wpf-degree/index").to_sym
+ end
+ 
+
+ 
