7c2371ba78ecd9f716c85ad70e019b695639db5f
Tania Barroso López <tbarrosolopez@gmail.com>
2016-12-22 15:39:47 -0500

    subiendo los estilos del layout

--- app.rb
+++ app.rb
@@ -1,12 +1,14 @@
 require 'sinatra'
-
-
  get '/index' do
       'Hi!!, Mis primeros pasos en Ruby - Sinatra'
  end
  get '/inicio' do
 	erb:'inicio', :layout =>("/wpf-degree/index").to_sym
  end
- 
-
- 
+#
+# get '/:locale/gallery' do
+#   @titulo = "Galeria"
+#   erb :"/views/wpf-degree/gallery" , :layout => :"/wpf-degree/index"
+#
+# end
+#
